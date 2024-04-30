188d4ebef0a134269d8d33587d146506de323354
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-04-30 20:50:58 -0300

    :sparkles: calendário e inserção de eventos finalizado - faltando edição e exclusão de eventos

--- app/frontend/client/src/ui/app/forms/Appointments/NewApointment/components/Form.tsx
+++ app/frontend/client/src/ui/app/forms/Appointments/NewApointment/components/Form.tsx
@@ -30,38 +30,49 @@ export default function Form({ data, setData, prevData, sendData}: FormProps) {
   } = useForm();
     const [clientSelected, setClientSelected] = useState("");
 
-  const submitForm = (formData) => {
-    const newAppointment = {
-      summary: formData.title,
-      location: "São Paulo - SP, Brasil",
-      description: formData.description,
-      start: {
-        dateTime: formData.appointmentDate,
-        timeZone: "America/Sao_Paulo",
-      },
-      end: {
-        dateTime: formData.appointmentDate,
-        timeZone: "America/Sao_Paulo",
-      },
-      attendees: [
-        {
-          email: formData.userAssociated,
-          optional: false,
+    const submitForm = (formData) => {
+      // Create a Date object from formData.appointmentDate
+      const startDateTime = new Date(formData.appointmentDate);
+
+      // Extract the hours and minutes from formData.appointmentTime
+      const [hours, minutes] = formData.appointmentTime.split(":").map(Number);
+
+      // Set the hours and minutes of startDateTime
+      startDateTime.setHours(hours, minutes);
+
+      // Create a Date object for the end time (1 hour after the start time)
+      const endDateTime = new Date(startDateTime.getTime() + 60 * 60 * 1000);
+
+      const newAppointment = {
+        summary: formData.title,
+        location: "São Paulo - SP, Brasil",
+        description: formData.description,
+        start: {
+          dateTime: startDateTime.toISOString(),
+          timeZone: "America/Sao_Paulo",
         },
-      ],
-      reminders: {
-        useDefault: false,
-        overrides: [
+        end: {
+          dateTime: endDateTime.toISOString(),
+          timeZone: "America/Sao_Paulo",
+        },
+        attendees: [
           {
-            method: "email",
-            minutes: 24 * 60,
+            email: formData.userAssociated,
+            optional: false,
           },
         ],
-      },
+        reminders: {
+          useDefault: false,
+          overrides: [
+            {
+              method: "email",
+              minutes: 24 * 60,
+            },
+          ],
+        },
+      };
+      setData(newAppointment);
     };
-    setData(newAppointment);
-    
-  };
 
   async function getClientList() {
     try {
